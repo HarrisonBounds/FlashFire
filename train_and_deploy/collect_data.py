2c3e75280597f6088ddf95920fc56806f45f11fd
HarrisonBounds <harrison.bounds777@gmail.com>
2023-10-31 15:14:27 -0500

    tuning steering

--- train_and_deploy/collect_data.py
+++ train_and_deploy/collect_data.py
@@ -57,7 +57,7 @@ start_time=datetime.now().strftime("%Y_%m_%d_%H_%M_")
 #Initialize motor and servo objects
 motor = PhaseEnableMotor(phase=19, enable=26)
 servo = Servo(24)
-center = 0.1
+center = 0.2
 offset = 0.3
 
 servo.value = center #start servo with wheels straight
